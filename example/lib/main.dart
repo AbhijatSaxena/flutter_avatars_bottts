5c138b24855a5fc8ba8835186b72436fac3fbc8d
Abhijat Saxena <abhijatsaxena@gmail.com>
2020-09-04 02:45:29 +0530

    Prefer dark mode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
